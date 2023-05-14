fbbaac3ddface735c6e60ea6e1ecd940f438991b
Pawel Jarosz <pawel.jarosz21@gmail.com>
2023-05-14 19:55:54 +0200

    Added candle, but snuffed, because failing light. Fixed animation of paper to inventory

--- assets/candle/candle.go
+++ assets/candle/candle.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "fire"
+  component: "/assets/candle/fire.particlefx"
+  position {
+    x: 0.0
+    y: 2.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
