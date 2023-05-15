ea2fbf3c838b7b3bcbc863dd6cc68c7f90238900
Pawel Jarosz <pawel.jarosz21@gmail.com>
2023-05-15 14:26:21 +0200

    Added more papers. Added walls around. Added compass.

---
+++ assets/wall/wall.go
@@ -0,0 +1,27 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad_2x2.dae\"\n"
+  "material: \"/illumination/materials/model.material\"\n"
+  "textures: \"/assets/wall/brick_color.png\"\n"
+  "textures: \"/illumination/textures/data.png\"\n"
+  "textures: \"/illumination/textures/empty.png\"\n"
+  "textures: \"/assets/wall/brick_color.png\"\n"
+  "textures: \"/assets/wall/brick_normal.png\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"{{NAME}}\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 1.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
